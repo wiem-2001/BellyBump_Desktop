4721ac10f98da570f3e8e7ac5cba7da430084ea1
bellyBump_Desktop <wiembm2001@gmail.com>
2024-03-25 11:50:33 +0100

    initial commit

---
+++ src/main/java/tn/esprit/Main.java
@@ -0,0 +1,12 @@
+package tn.esprit;
+
+import tn.esprit.util.MaConnexion;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        MaConnexion cnx = MaConnexion.getInstance();
+    }
+}
